86b1438f1ca6b945c97c6e8d99665b8e9563ce19
Tim <timothy.theis@gmail.com>
2026-04-11 14:10:56 -0400

    Revert "Add independent Libre LinkUp Direct plugin"
    This reverts commit bfb138e51929981dd4b8bb96efa547774e7c8930.

--- LibreLinkUpCGMPlugin/LibreLinkUpCGMPlugin.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-import LoopKitUI
-
-class LibreLinkUpCGMPlugin: NSObject, CGMManagerUIPlugin {
-    public var cgmManagerType: CGMManagerUI.Type? {
-        return LibreLinkUpManager.self
-    }
-}
